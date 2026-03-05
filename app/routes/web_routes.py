22561bf386c1b9cb2512b534d89fa5dc91feb827
Enzo de Almeida Sanchez <enzosanchez2302@gmail.com>
2026-03-05 13:44:21 -0300

    Criado uma função para salvar nos Cookies o ID do user atual
    Agora quando o usuario loga, seu ID é salvo nos Cookies da pagina.

--- app/routes/web_routes.py
+++ app/routes/web_routes.py
@@ -1,18 +1,32 @@
-from fastapi import APIRouter, Request, Depends, Form
+from fastapi import APIRouter, Request, Depends, Form, Cookie
 from fastapi.templating import Jinja2Templates
 from fastapi.responses import HTMLResponse, RedirectResponse
 from app.database.database import get_db
 from sqlalchemy.orm import Session
 from app.services.user_services import criar_usuario, logar_usuario
+from app.models.user import User
 
 router = APIRouter()
 templates = Jinja2Templates(directory="app/templates")
 
+
+#============================Funções Auxiliares================================
+def get_current_user(user_id:str = Cookie(None), db:Session = Depends(get_db)):
+    if not user_id:
+        return None
+
+    return db.query(User).filter_by(id=int(user_id)).first()
+#==============================================================================
+
+
+
 #==============================End-Points Home===========================
 @router.get("/")
-def home_page(request:Request):
-    return templates.TemplateResponse("index.html", {"request":request})
+def home_page(request:Request, user=Depends(get_current_user)):
+    return templates.TemplateResponse("index.html", {"request":request, "user": user})
 #========================================================================
+
+
 
 
 #===========================================End-Points Register==================================================
@@ -27,6 +41,8 @@ def register_user(request:Request, username=Form(), email=Form(), password=Form(
 #=================================================================================================================
 
 
+
+
 #===========================================End-Points Login==================================
 @router.get("/login")
 def login_page(request:Request):
@@ -34,11 +50,19 @@ def login_page(request:Request):
 
 @router.post("/login")
 def login_user(request:Request, email=Form(), password=Form(), db:Session = Depends(get_db)):
-    if (logar_usuario(db, email, password)):
-        return RedirectResponse(url="/", status_code=303)
+    user = logar_usuario(db, email, password)
+    if (user):
+        response = RedirectResponse(url="/", status_code=303)
+        response.set_cookie(
+            key="user_id",
+            value=str(user.id),
+            httponly=True
+            )
+        
+        return response
+     
     return templates.TemplateResponse("login.html", {"request":request})
 
 #=============================================================================================
 
 
-    
