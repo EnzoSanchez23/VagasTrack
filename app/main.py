81ddb00193588097f235aeb0b6699b110f04e8b5
Enzo de Almeida Sanchez <enzosanchez2302@gmail.com>
2026-03-07 19:08:08 -0300

    Agora cria um usuario ADMIN quando o banco é criado.

--- app/main.py
+++ app/main.py
@@ -1,11 +1,26 @@
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
-from app.database.database import Base, engine
+from app.database.database import Base, engine, SessionLocal
 from app.routes.web_routes import router
+from app.models.user import User
+from dotenv import load_dotenv
+import os
 
 app = FastAPI()
+load_dotenv()
 
 Base.metadata.create_all(bind=engine)
+
+db = SessionLocal()
+admin_user = os.getenv("ADMIN_USER")
+admin_email = os.getenv("ADMIN_EMAIL")
+admin_password = os.getenv("ADMIN_PASSWORD")
+
+admin = db.query(User).filter_by(nome_usuario=admin_user,email=admin_email).first()
+if not admin:
+    admin = User(nome_usuario=admin_user, email=admin_email, senha=admin_password)
+    db.add(admin)
+    db.commit()
 
 # Servir arquivos estáticos
 app.mount("/static", StaticFiles(directory="app/static"), name="static")
